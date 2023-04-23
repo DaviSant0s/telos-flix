1c81b1ab4e967969f723b6a1f13bd70eea6fd9fb
Davi Santos <daviir17@gmail.com>
2023-04-23 13:08:52 -0300

    fix(componentes-desnecessarios): exclusão de alguns componentes que não vão ser usados na aplicação

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Body from '../body'
 
 
 function Header() {
--- src/components/titulo/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Titulo() {
-  return <p>Davi Santos</p>
-}
-
-export default Titulo
