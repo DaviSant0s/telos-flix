4cc813235cd30c26272664712b6a5cd21d7f5f40
Davi Santos <daviir17@gmail.com>
2023-05-03 15:39:38 -0300

    feat(trending): criação da componente Trending

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import React from 'react'
 import MainVideo from '../../components/mainVideo';
+import Trending from '../../components/trending';
 
 function Home() {
   return (
     <div>
       <MainVideo/>
+      <Trending/>
     </div>
   )
 }
