d9b692da7e43531b4d7a1437e438bfc15cba28ab
Davi Santos <daviir17@gmail.com>
2023-05-05 18:13:41 -0300

    feat(minicards): criação e estiliação dos minicards

---
+++ src/components/miniVideoCard/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+export default function MiniVideoCard( {title} ) {
+  return (
+    <div className='miniVideoCard'>{title}</div>
+  )
+}
--- src/components/trending/index.jsx
+++ src/components/trending/index.jsx
@@ -1,11 +1,33 @@
 import { SignalCellularAltOutlined } from '@mui/icons-material'
 import React from 'react'
 import './index.css'
+import MiniVideoCard from '../miniVideoCard'
 
 export default function Trending() {
+  const videos = [
+    {
+      title: "Video 1"
+    },
+    {
+      title: "Video 2"
+    },
+    {
+      title: "Video 3"
+    }
+  ]
+
   return (
     <div className='trendingSection'>
-        <div className='labelSection'><SignalCellularAltOutlined/> Em alta</div>
+        <div className='labelSection'>
+          <SignalCellularAltOutlined/> Em alta
+        </div>
+
+        <div className='trendingVideosGrid'>
+          {videos.map((video) => (
+              <MiniVideoCard title={video.title}/>
+          ))}
+        </div>
+        
     </div>
   )
 }
