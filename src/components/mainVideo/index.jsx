830434d090474e4fa34815da800903d860692d46
Davi Santos <daviir17@gmail.com>
2023-05-06 14:04:47 -0300

    style(icones dos cards): espaçamento de 10px

--- src/components/mainVideo/index.jsx
+++ src/components/mainVideo/index.jsx
@@ -3,6 +3,8 @@ import image from './Hero.png'
 
 export default function MainVideo() {
   return (
-    <div style={{backgroundColor: 'transparent'}}><img src={image} alt="" /></div>
+    <div style={{backgroundColor: 'transparent', marginTop: '284px'}}>
+      <img src={image} alt="" />
+    </div>
   )
 }
